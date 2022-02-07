3b1d870826629d7d1b9667c4d18b7eca02bf7451
BigFancyBen <benadamsdroid@gmail.com>
2022-02-06 22:54:10 -0700

    clone and fix and init

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -17,6 +17,7 @@ function WebPlayback(props) {
     const [is_paused, setPaused] = useState(false);
     const [is_active, setActive] = useState(false);
     const [player, setPlayer] = useState(undefined);
+    const [device, setDevice] = useState(false);
     const [current_track, setTrack] = useState(track);
 
     useEffect(() => {
@@ -30,7 +31,7 @@ function WebPlayback(props) {
         window.onSpotifyWebPlaybackSDKReady = () => {
 
             const player = new window.Spotify.Player({
-                name: 'Web Playback SDK',
+                name: 'Jukebox',
                 getOAuthToken: cb => { cb(props.token); },
                 volume: 0.5
             });
@@ -38,7 +39,8 @@ function WebPlayback(props) {
             setPlayer(player);
 
             player.addListener('ready', ({ device_id }) => {
-                console.log('Ready with Device ID', device_id);
+                //console.log('Ready with Device ID', device_id);
+                setDevice(device_id);
             });
 
             player.addListener('not_ready', ({ device_id }) => {
@@ -61,19 +63,87 @@ function WebPlayback(props) {
             }));
 
             player.connect();
-
         };
     }, []);
 
+    useEffect(() => {
+        if(device == false ){return;}
+        fetch(`https://api.spotify.com/v1/me/player`, {
+            "method": "PUT",
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${props.token}`
+            },
+            "body":JSON.stringify({device_ids: [device]})
+        })
+    }, [device]);
+    
+    function hooch(){
+
+        // fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device}`, {
+        //     method: 'PUT',
+        //     body: JSON.stringify({ uris: "spotify:album:39XRAKlA9dJ6wCVEDGxaDe" }),
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': `Bearer ${props.token}`
+        //     },
+        // });
+
+        fetch("https://api.spotify.com/v1/albums/1Ki56K82avE7nTkZEyVIE7", {
+            "method": "GET",
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${props.token}`
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
+            // const tracks = data.tracks.items.map(obj => {
+            //     return obj.uri
+            // })
+            queueAlbum(data.tracks.items);
+        })
+        .catch(err => {
+            console.error(err);
+        });              
+    }
+
+    async function queueAlbum(songs){
+        for(let i=0;i<songs.length;i++){
+            await queueSong(songs[i].uri);
+        }
+
+    }
+
+    async function queueSong(songUri){
+        let response = await fetch(`https://api.spotify.com/v1/me/player/queue?uri=${songUri}`, {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${props.token}`
+            }
+        })
+        const waitFor = delay => new Promise(resolve => setTimeout(resolve, delay));
+        await waitFor(500);
+        return await response;
+    }
+
+    useEffect(() => {
+        console.log(device," ^^^^^ ", props.token);
+    }, [device]);
+
+
     if (!is_active) { 
         return (
-            <>
+            <div>
                 <div className="container">
                     <div className="main-wrapper">
                         <b> Instance not active. Transfer your playback using your Spotify app </b>
                     </div>
                 </div>
-            </>)
+            </div>)
     } else {
         return (
             <>
@@ -96,6 +166,9 @@ function WebPlayback(props) {
 
                             <button className="btn-spotify" onClick={() => { player.nextTrack() }} >
                                 &gt;&gt;
+                            </button>
+                            <button className="btn-spotify" onClick={() => { hooch() }} >
+                                play album
                             </button>
                         </div>
                     </div>
